092297529050a7b538d565698a81c30ede060aa3
abhay-byte <abhay02delhi@gmail.com>
2026-02-15 09:18:13 +0530

    Refactor comparison page Alpine.js logic to dedicated file to fix SPA navigation issues

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,5 +3,10 @@ import './route-loader';
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
+import comparisonPage from './components/comparison-page';
+
+document.addEventListener('alpine:init', () => {
+    Alpine.data('comparisonPage', comparisonPage);
+});
 
 Alpine.start();
